919073cd2faf3cc8e87ad2f12a0f73ffd16ecaba
Harsh Patidar <harshptidar1234@gmail.com>
2025-06-24 15:27:37 +0530

    Fixed Login issue 4

--- web/phone/phone-temp/src/api/api.js
+++ web/phone/phone-temp/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://fuznex.onrender.com/';
+const BASE_URL ='https://fuznex.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
